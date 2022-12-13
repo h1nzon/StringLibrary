d7bf676ddafde6ab60bf34285ab28b46f0e1f3a2
h1nzon <79138295+h1nzon@users.noreply.github.com>
2022-12-13 10:00:23 +0500

    Добавьте файлы проекта.

---
+++ StringLibrary/StringCheckClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLibrary
+{
+    public class StringCheckClass
+    {
+        /// <summary>
+        /// Возвращение букв в лист
+        /// </summary>
+        /// <param name="textString"></param>
+        /// <returns></returns>
+        public static List<char> GetLetters(string textString)
+        {
+            List<char> list = new List<char>();
+            
+            return list;
+        }
+    }
+}
